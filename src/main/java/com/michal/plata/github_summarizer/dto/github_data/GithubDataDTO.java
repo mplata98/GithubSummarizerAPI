ceac09b3a2ff0c64275432e7f93e0206557f2b01
Michal Plata <mplata98@gmail.com>
2021-12-23 14:13:32 +0100

    Added empty GithubDataDTO implementation

---
+++ src/main/java/com/michal/plata/github_summarizer/dto/github_data/GithubDataDTO.java
@@ -0,0 +1,8 @@
+package com.michal.plata.github_summarizer.dto.github_data;
+
+import lombok.Data;
+
+@Data
+public interface GithubDataDTO {
+    
+}
---
+++ src/main/java/com/michal/plata/github_summarizer/dto/github_data/GithubDataDTOImpl.java
@@ -0,0 +1,5 @@
+package com.michal.plata.github_summarizer.dto.github_data;
+
+public class GithubDataDTOImpl implements GithubDataDTO {
+    
+}
