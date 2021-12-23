cdd652ce086273aef3cfdb04a6559b11ecbe2670
Michal Plata <mplata98@gmail.com>
2021-12-23 12:48:26 +0100

    Initial commit

---
+++ src/main/java/com/michal/plata/gitHubSummarizer/GitHubSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.michal.plata.gitHubSummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michal/plata/gitHubSummarizer/GitHubSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michal.plata.gitHubSummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
