9e49fdf277401334302d2ba345199f66e87d6a95
Michal Plata <mplata98@gmail.com>
2021-12-30 13:41:55 +0100

    Renamed Printer.class to DebugPrinter.class  for claryfication

--- src/main/java/com/michal/plata/github_summarizer/support/printer/DebugPrinter.java
+++ src/main/java/com/michal/plata/github_summarizer/support/printer/DebugPrinter.java
@@ -4,13 +4,13 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Printer {
+public class DebugPrinter {
 
-    private Printer() {
+    private DebugPrinter() {
     }
 
     public static void print(String string) {
-        Logger logger = LogManager.getLogger(Printer.class);
+        Logger logger = LogManager.getLogger(DebugPrinter.class);
         logger.log(Level.INFO, "{} {} {}", "\u001B[1;32m", string, "\u001B[0m");
     }
 }
